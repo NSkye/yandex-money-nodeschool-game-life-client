030553c70a7695f15b43e11bded2770065f33e4b
nikolai-laevskii <nikolai.laevskii@yandex.ru>
2017-10-29 01:36:18 +0300

    Добавлен минимальный функционал

--- main/index.js
+++ main/index.js
@@ -1,5 +1,4 @@
 'use strict';
-
 //
 // YOUR CODE GOES HERE...
 //
@@ -20,3 +19,31 @@
 //
 // Nyan cat lies here...
 //
+
+let gameInstance = null;
+const types = {
+  "INITIALIZE": (data, socket) => {
+    gameInstance = new LifeGame(data.user, data.settings);
+    gameInstance.init();
+    gameInstance.setState(data.state);
+    gameInstance.send = data => {
+      const type = 'ADD_POINT';
+      socket.send(JSON.stringify({type, data}));
+    };
+  },
+  "UPDATE_STATE": data => {
+    gameInstance.setState(data);
+  }
+};
+
+App.onToken = function (token) {
+  const socket = new WebSocket(`ws://ws.rudenko.tech/life/api/?token=${token}`);
+  socket.onopen = () => {
+    console.log(`Connection established with token=${token}`);
+  }
+  socket.onmessage = (msg) => {
+    const msgdata = JSON.parse(msg.data);
+    console.log(msgdata);
+    types[msgdata.type](msgdata.data, socket);
+  }
+}
