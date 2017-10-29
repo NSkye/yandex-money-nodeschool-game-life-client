6b36504edd2929ea44abcc61d84c31bf878cae5a
nikolai-laevskii <nikolai.laevskii@yandex.ru>
2017-10-29 21:58:16 +0300

    Рефакторинг

--- main/index.js
+++ main/index.js
@@ -21,55 +21,60 @@
 // Nyan cat lies here...
 //
 let gameInstance = null;
-const types = {
-  "INITIALIZE": (data, socket) => {
-    gameInstance = new LifeGame(data.user, data.settings);
-    gameInstance.init();
-    gameInstance.setState(data.state);
-    gameInstance.send = data => {
-      const type = 'ADD_POINT';
-      socket.send(JSON.stringify({type, data}));
-    };
-  },
-  "UPDATE_STATE": data => {
-    gameInstance.setState(data);
+const handleToken = token => {
+  const addr = {
+    local: `ws://localhost:3001`,
+    network: `ws://192.168.0.92:3001`,
+    remote: `ws://ws.rudenko.tech/life/api`
   }
-};
 
-const createSocket = (url, params) => {
-  const keys = Object.keys(params);
-  const path = keys.map(key => `${key}=${params[key]}`).join('&');
-  return new WebSocket(url+'/?'+path);
-}
-const addr = {
-  local: `ws://localhost:3001`,
-  network: `ws://192.168.0.92:3001`,
-  remote: `ws://ws.rudenko.tech/life/api`
-}
-App.onToken = function (token) {
-  const socket = createSocket(addr.local, {token});
-  socket.onopen = () => {
-    console.log(`Connection established with token=${token}`);
-  }
-  socket.onmessage = (msg) => {
-    try {
-      const msgdata = JSON.parse(msg.data);
-      if (!types[msgdata.type]) {
-        throw new Error(`Message type error. Expected one of following types: ${Object.keys(types)}; got: ${msgdata.type}`);
+  const handleOpen = () => console.log(`Connection established. ${token}`);
+  const handleMessage = msg => {
+    const executeScenario = (msgdata, socket) => {
+      const allowedTypes = ['INITIALIZE', 'UPDATE_STATE'];
+      const type = msgdata.type;
+      const data = msgdata.data;
+      switch (type) {
+        case 'INITIALIZE':
+          gameInstance = new LifeGame(data.user, data.settings);
+          gameInstance.init();
+          gameInstance.setState(data.state);
+          gameInstance.send = (data) => {
+            const type = 'ADD_POINT';
+            socket.send(JSON.stringify({type, data}));
+          };
+          break;
+        case 'UPDATE_STATE':
+          gameInstance.setState(data);
+          break;
+        default:
+          throw new Error(`Message type error. Expected one of following types: ${allowedTypes}; got: ${type}`);
       }
-      types[msgdata.type](msgdata.data, socket);
+    }
+    try {
+      executeScenario(JSON.parse(msg.data), socket);
     } catch (e) {
       return console.log(e.message);
     }
   }
-  socket.onerror = error => {
+  const handleError = error => {
     console.log('Socket error: ', error);
   }
-  socket.onclose = event => {
-    console.log(`
-      Connection closed.
-      Code: ${event.code},
-      Reason: ${event.reason}`);
+  const handleClose = event => {
+    console.log(`Connection closed.\nCode: ${event.code},\nReason: ${event.reason}`);
     gameInstance = null;
   }
+  const createSocket = (url, params) => {
+    const keys = Object.keys(params);
+    const path = keys.map(key => `${key}=${params[key]}`).join('&');
+    return new WebSocket(url+'/?'+path);
+  }
+
+  const socket = createSocket(addr.local, {token});
+  socket.onopen = handleOpen;
+  socket.onmessage = handleMessage;
+  socket.onerror = handleError;
+  socket.onclose = handleClose;
 }
+
+App.onToken = handleToken;
